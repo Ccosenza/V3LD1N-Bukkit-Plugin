f5f63da8f277a624d062937c1351be2bbd76a361
Ccosenza <ccosenza2@gmail.com>
2015-01-12 14:00:33 -0500

    Add teleport-location-particles and teleport-location-sounds lists

--- src/com/v3ld1n/tasks/TeleportTask.java
+++ src/com/v3ld1n/tasks/TeleportTask.java
@@ -1,6 +1,5 @@
 package com.v3ld1n.tasks;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Location;
@@ -21,36 +20,37 @@ public class TeleportTask extends Task {
     public void run() {
         Location location = this.getLocationSetting("location");
         double radius = this.getDoubleSetting("radius");
-        Location teleportTo = this.getLocationSetting("teleport-location");
+        Location teleportLoc = this.getLocationSetting("teleport-location");
         Location particleLocation = null;
         boolean playAnimation = false;
 
         if (this.getSetting("particle-location") != null) {
             particleLocation = this.getLocationSetting("particle-location");
         } else {
-            particleLocation = teleportTo;
+            particleLocation = location;
         }
 
         if (this.getBooleanSetting("animation")) {
             playAnimation = true;
         }
 
-        List<Particle> particles = new ArrayList<>();
-        for (String particleString : this.getStringListSetting("particles")) {
-            particles.add(Particle.fromString(particleString));
-        }
-
         List<Player> players = WorldUtil.getNearbyPlayers(location, radius);
         for (Player player : players) {
-            player.teleport(teleportTo);
+            player.teleport(teleportLoc);
             if (playAnimation) {
                 PlayerAnimation.BED_LEAVE.playToPlayer(player);
             }
             for (String particle : this.getStringListSetting("particles")) {
                 Particle.fromString(particle).display(particleLocation);
             }
+            for (String particle : this.getStringListSetting("teleport-location-particles")) {
+                Particle.fromString(particle).display(teleportLoc);
+            }
             for (String sound : this.getStringListSetting("sounds")) {
-                SoundUtil.playSoundString(sound, teleportTo);
+                SoundUtil.playSoundString(sound, location);
+            }
+            for (String sound : this.getStringListSetting("teleport-location-sounds")) {
+                SoundUtil.playSoundString(sound, teleportLoc);
             }
         }
     }
