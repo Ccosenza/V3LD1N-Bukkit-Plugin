6b8ab79f99a0cce6536d1a34dc44e7b55ed8c79b
Ccosenza <ccosenza2@gmail.com>
2015-01-11 23:50:49 -0500

    Add warp sounds

--- src/com/v3ld1n/commands/WarpCommand.java
+++ src/com/v3ld1n/commands/WarpCommand.java
@@ -6,7 +6,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.v3ld1n.Message;
-import com.v3ld1n.util.ConfigUtil;
+import com.v3ld1n.V3LD1N;
 import com.v3ld1n.util.Particle;
 import com.v3ld1n.util.SoundUtil;
 
@@ -15,14 +15,22 @@ public class WarpCommand extends V3LD1NCommand {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
-            Bukkit.getServer().dispatchCommand(sender, "warp " + label);
-            for (Particle particle : ConfigUtil.getWarpParticles(label)) {
-                particle.display(p.getLocation(), p);
+            Warp warp = null;
+            for (Warp warpObject : V3LD1N.getWarps()) {
+                if (warpObject.getName().equalsIgnoreCase(label)) {
+                    warp = warpObject;
+                }
             }
-            for (String sound : ConfigUtil.getWarpSounds(label)) {
-                SoundUtil.playSoundString(sound, p.getLocation());
+            if (warp != null) {
+                Bukkit.getServer().dispatchCommand(sender, "warp " + label);
+                for (Particle particle : warp.getParticles()) {
+                    particle.display(p.getLocation(), p);
+                }
+                for (String sound : warp.getSounds()) {
+                    SoundUtil.playSoundString(sound, p.getLocation());
+                }
+                return true;
             }
-            return true;
         }
         sender.sendMessage(Message.COMMAND_NOT_PLAYER.toString());
         return true;
