0c3b6fc596dc12af9058fddbd0395be16447654a
Ccosenza <ccosenza2@gmail.com>
2015-01-12 00:11:29 -0500

    Add /playerlist ping

--- src/com/v3ld1n/commands/PlayerListCommand.java
+++ src/com/v3ld1n/commands/PlayerListCommand.java
@@ -3,6 +3,7 @@ package com.v3ld1n.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
+import com.v3ld1n.ConfigSetting;
 import com.v3ld1n.Message;
 import com.v3ld1n.util.ChatUtil;
 import com.v3ld1n.util.ConfigUtil;
@@ -11,6 +12,7 @@ public class PlayerListCommand extends V3LD1NCommand {
     public PlayerListCommand() {
         this.addUsage("set <header> <footer>", "Set the header and footer");
         this.addUsage("reset", "Reset the header and footer");
+        this.addUsage("ping", "Toggle player list ping display");
     }
 
     @Override
@@ -30,6 +32,16 @@ public class PlayerListCommand extends V3LD1NCommand {
                 if (args[0].equalsIgnoreCase("reset")) {
                     ConfigUtil.setPlayerListHeaderFooter("{text:\"\"}", "{text:\"\"}");
                     ChatUtil.sendMessage(sender, Message.PLAYERLIST_RESET.toString(), 2);
+                    return true;
+                } else if (args[0].equalsIgnoreCase("ping")) {
+                    Message message = null;
+                    if (ConfigSetting.PLAYER_LIST_PING_ENABLED.getBoolean()) {
+                        message = Message.PLAYERLIST_DISABLE_PING;
+                    } else {
+                        message = Message.PLAYERLIST_ENABLE_PING;
+                    }
+                    ConfigUtil.toggleSetting(ConfigSetting.PLAYER_LIST_PING_ENABLED);
+                    sender.sendMessage(message.toString());
                     return true;
                 }
             }
