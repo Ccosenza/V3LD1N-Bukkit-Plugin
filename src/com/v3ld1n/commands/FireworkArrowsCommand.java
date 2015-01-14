95555b156f7aa09475738862ee76bab9c1214319
Ccosenza <ccosenza2@gmail.com>
2015-01-14 02:29:21 -0500

    Rewrite

--- src/com/v3ld1n/commands/FireworkArrowsCommand.java
+++ src/com/v3ld1n/commands/FireworkArrowsCommand.java
@@ -1,11 +1,15 @@
 package com.v3ld1n.commands;
 
+import java.util.Arrays;
+
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.v3ld1n.Message;
 import com.v3ld1n.PlayerData;
+import com.v3ld1n.util.StringUtil;
 
 public class FireworkArrowsCommand extends V3LD1NCommand {
     public FireworkArrowsCommand() {
@@ -16,32 +20,20 @@ public class FireworkArrowsCommand extends V3LD1NCommand {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player) {
             Player p = (Player) sender;
-            if (args.length > 0) {
-                String type = args[0].toUpperCase();
-                switch (type) {
-                case "BALL":
+            if (args.length == 1) {
+                String typeString = args[0].toUpperCase();
+                Type type = Type.valueOf(typeString);
+                if (type == Type.BALL) {
                     PlayerData.FIREWORK_ARROWS.set(p.getUniqueId(), null);
-                    p.sendMessage(String.format(Message.FIREWORKARROWS_SET.toString(), "Small Ball"));
-                    break;
-                case "BALL_LARGE":
-                    PlayerData.FIREWORK_ARROWS.set(p.getUniqueId(), args[0].toUpperCase());
-                    p.sendMessage(String.format(Message.FIREWORKARROWS_SET.toString(), "Large Ball"));
-                    break;
-                case "STAR":
-                    PlayerData.FIREWORK_ARROWS.set(p.getUniqueId(), args[0].toUpperCase());
-                    p.sendMessage(String.format(Message.FIREWORKARROWS_SET.toString(), "Star"));
-                    break;
-                case "CREEPER":
-                    PlayerData.FIREWORK_ARROWS.set(p.getUniqueId(), args[0].toUpperCase());
-                    p.sendMessage(String.format(Message.FIREWORKARROWS_SET.toString(), "Creeper"));
-                    break;
-                case "BURST":
-                    PlayerData.FIREWORK_ARROWS.set(p.getUniqueId(), args[0].toUpperCase());
-                    p.sendMessage(String.format(Message.FIREWORKARROWS_SET.toString(), "Burst"));
-                    break;
-                default:
-                    p.sendMessage(Message.FIREWORKARROWS_INVALID_SHAPE.toString());
+                } else {
+                    if (Arrays.asList(Type.values()).contains(type)) {
+                        PlayerData.FIREWORK_ARROWS.set(p.getUniqueId(), args[0].toUpperCase());
+                    } else {
+                        p.sendMessage(Message.FIREWORKARROWS_INVALID_SHAPE.toString());
+                        return true;
+                    }
                 }
+                p.sendMessage(String.format(Message.FIREWORKARROWS_SET.toString(), StringUtil.fromEnum(type, true)));
                 return true;
             }
             this.sendUsage(sender, label, command);
--- src/com/v3ld1n/commands/RatchetsBowCommand.java
+++ src/com/v3ld1n/commands/RatchetsBowCommand.java
@@ -1,14 +1,16 @@
 package com.v3ld1n.commands;
 
+import java.util.Arrays;
+
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.v3ld1n.ConfigSetting;
 import com.v3ld1n.Message;
 import com.v3ld1n.PlayerData;
-import com.v3ld1n.util.Particle;
+import com.v3ld1n.items.ratchet.RatchetBowType;
+import com.v3ld1n.util.StringUtil;
 
 public class RatchetsBowCommand extends V3LD1NCommand {
     public RatchetsBowCommand() {
@@ -20,58 +22,22 @@ public class RatchetsBowCommand extends V3LD1NCommand {
         if (sender instanceof Player) {
             Player p = (Player) sender;
             Location loc = p.getEyeLocation();
-            if (args.length > 0) {
+            if (args.length == 1) {
                 String projectile = args[0].toLowerCase();
-                switch (projectile) {
-                    case "fireball":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), null);
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_FIREBALL.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Fireball"));
-                        return true;
-                    case "arrow":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_ARROW.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Arrow"));
-                        return true;
-                    case "triplearrows":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_TRIPLE_ARROWS.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Triple Arrows"));
-                        return true;
-                    case "fireworkarrow":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_FIREWORK.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Firework Arrow"));
-                        return true;
-                    case "snowball":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_SNOWBALL.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Snowball"));
-                        return true;
-                    case "enderpearl":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_ENDER_PEARL.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Ender Pearl"));
-                        return true;
-                    case "egg":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_EGG.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Egg"));
-                        return true;
-                    case "witherskull":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_SKULL.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Wither Skull"));
-                        return true;
-                    case "bluewitherskull":
-                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toLowerCase());
-                        Particle.fromString(ConfigSetting.PARTICLE_RATCHETSBOW_BLUE_SKULL.getString()).display(loc);
-                        p.sendMessage(String.format(Message.RATCHETSBOW_SET.toString(), "Blue Wither Skull"));
-                        return true;
-                    default:
+                RatchetBowType type = RatchetBowType.valueOf(projectile);
+                if (type == RatchetBowType.FIREBALL) {
+                    PlayerData.RATCHETS_BOW.set(p.getUniqueId(), null);
+                } else {
+                    if (Arrays.asList(RatchetBowType.values()).contains(type)) {
+                        PlayerData.RATCHETS_BOW.set(p.getUniqueId(), args[0].toUpperCase());
+                    } else {
                         p.sendMessage(Message.RATCHETSBOW_INVALID_PROJECTILE.toString());
                         return true;
+                    }
                 }
+                type.getParticle().display(loc);
+                p.sendMessage(String.format(Message.FIREWORKARROWS_SET.toString(), StringUtil.fromEnum(type, true)));
+                return true;
             }
             this.sendUsage(sender, label, command);
             return true;
