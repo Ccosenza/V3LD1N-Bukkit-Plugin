db7257e5797d657f166fa8bdf0942efb46cf1542
Ccosenza <ccosenza2@gmail.com>
2015-01-18 23:45:56 -0500

    Add Sign

---
+++ src/com/v3ld1n/blocks/Sign.java
@@ -0,0 +1,42 @@
+package com.v3ld1n.blocks;
+
+import java.util.List;
+
+import com.v3ld1n.util.Particle;
+import com.v3ld1n.util.Sound;
+
+public class Sign {
+    String text;
+    List<String> playerCommands;
+    List<String> consoleCommands;
+    List<Particle> particles;
+    List<Sound> sounds;
+
+    public Sign(String text, List<String> playerCommands, List<String> consoleCommands, List<Particle> particles, List<Sound> sounds) {
+        this.text = text;
+        this.playerCommands = playerCommands;
+        this.consoleCommands = consoleCommands;
+        this.particles = particles;
+        this.sounds = sounds;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public List<String> getPlayerCommands() {
+        return playerCommands;
+    }
+
+    public List<String> getConsoleCommands() {
+        return consoleCommands;
+    }
+
+    public List<Particle> getParticles() {
+        return particles;
+    }
+
+    public List<Sound> getSounds() {
+        return sounds;
+    }
+}
