3fbfb13bae518b1108065429cb2dd9fb5406442c
Ccosenza <ccosenza2@gmail.com>
2015-04-14 17:17:37 -0400

    Smelt dropped items

--- src/com/v3ld1n/items/ratchet/RatchetPickaxe.java
+++ src/com/v3ld1n/items/ratchet/RatchetPickaxe.java
@@ -1,11 +1,20 @@
 package com.v3ld1n.items.ratchet;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
+import com.v3ld1n.V3LD1N;
 import com.v3ld1n.items.V3LD1NItem;
+import com.v3ld1n.util.ItemUtil;
+import com.v3ld1n.util.Particle;
+import com.v3ld1n.util.WorldUtil;
 
 public class RatchetPickaxe extends V3LD1NItem {
     public RatchetPickaxe() {
@@ -16,8 +25,23 @@ public class RatchetPickaxe extends V3LD1NItem {
     public void onBlockBreak(BlockBreakEvent event) {
         if (this.equalsItem(event.getPlayer().getItemInHand())) {
             Block block = event.getBlock();
-            Location loc = new Location(block.getWorld(), block.getLocation().getX() + 0.5, block.getLocation().getY() + 0.5, block.getLocation().getZ() + 0.5);
-            this.getParticleSetting("particle").display(loc);
+            final Location loc = new Location(block.getWorld(), block.getLocation().getX() + 0.5, block.getLocation().getY() + 0.5, block.getLocation().getZ() + 0.5);
+            for (String particle : this.getStringListSetting("particles")) {
+                Particle.fromString(particle).display(loc);
+            }
+            Bukkit.getServer().getScheduler().runTaskLater(V3LD1N.getPlugin(), new Runnable() {
+                @Override
+                public void run() {
+                    for (Entity entity : WorldUtil.getNearbyEntities(loc, 2)) {
+                        if (entity.getType() == EntityType.DROPPED_ITEM) {
+                            Item item = (Item) entity;
+                            ItemStack smeltedStack = ItemUtil.smelt(item.getItemStack());
+                            item.remove();
+                            loc.getWorld().dropItemNaturally(loc, smeltedStack);
+                        }
+                    }
+                }
+            }, 1);
         }
     }
 }
