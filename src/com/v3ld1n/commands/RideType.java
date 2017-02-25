7c7a5c46c9009261efd6c6b67616cbc8838035ba
Ccosenza <ccosenza2@gmail.com>
2017-02-25 13:24:40 -0500

    Added messages

--- src/com/v3ld1n/commands/RideType.java
+++ src/com/v3ld1n/commands/RideType.java
@@ -1,6 +1,18 @@
 package com.v3ld1n.commands;
 
+import com.v3ld1n.Message;
+
 public enum RideType {
-    RIDE,
-    HOLD;
+    RIDE(Message.get("ride-ride")),
+    HOLD(Message.get("ride-hold"));
+
+    private final Message message;
+
+    private RideType(Message message) {
+        this.message = message;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
 }
