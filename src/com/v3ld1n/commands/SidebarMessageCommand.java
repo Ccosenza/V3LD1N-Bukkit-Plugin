a9a4163e3cedbf7236a3e760ab75b07b503624c5
Ccosenza <ccosenza2@gmail.com>
2017-02-26 19:13:32 -0500

    Rewrite

--- src/com/v3ld1n/commands/SidebarMessageCommand.java
+++ src/com/v3ld1n/commands/SidebarMessageCommand.java
@@ -1,15 +1,11 @@
 package com.v3ld1n.commands;
 
-import java.util.Arrays;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.v3ld1n.Message;
-import com.v3ld1n.util.ChatUtil;
-import com.v3ld1n.util.MessageType;
 import com.v3ld1n.util.SidebarMessage;
 import com.v3ld1n.util.StringUtil;
 
@@ -22,39 +18,41 @@ public class SidebarMessageCommand extends V3LD1NCommand {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender.hasPermission("v3ld1n.sidebarmessage")) {
-            if (args.length >= 3) {
-                int time;
-                String timeArg = args[0];
-                try {
-                    if (timeArg.charAt(timeArg.length() - 1) == 's') {
-                        time = Integer.parseInt(timeArg.substring(0, args[0].length() - 1)) * 20;
-                    } else {
-                        time = Integer.parseInt(timeArg);
-                    }
-                } catch (Exception IllegalArgumentException) {
-                    String invalidTime = Message.get("sidebarmessage-invalid-time").toString();
-                    sender.sendMessage(String.format(invalidTime, DEFAULT_TIME, DEFAULT_TIME / 20));
-                    time = DEFAULT_TIME;
-                }
-                SidebarMessage sbm = new SidebarMessage(args[1]);
-                for (String arg : args) {
-                    if (Arrays.asList(args).indexOf(arg) > 1) {
-                        sbm.addLine(arg);
-                    }
-                }
-                for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-                    sbm.display(p, time);
-                }
-                String replaced = StringUtil.formatText(sbm.getTitle().replaceAll("_", " "));
-                String message = String.format(Message.get("sidebarmessage-display").toString(), replaced);
-                ChatUtil.sendMessage(sender, message, MessageType.ACTION_BAR);
-                return true;
-            }
+        if (sendPermissionMessage(sender, "v3ld1n.sidebarmessage")) return true;
+
+        if (args.length < 3) {
             this.sendUsage(sender);
             return true;
         }
-        sender.sendMessage(Message.get("command-permission").toString());
+
+        display(args, sender);
         return true;
+    }
+
+    private int getTicks(String timeArg, CommandSender user) {
+        int ticks;
+        try {
+            if (timeArg.charAt(timeArg.length() - 1) != 's') {
+                ticks = Integer.parseInt(timeArg);
+            }
+            ticks = Integer.parseInt(timeArg.substring(0, timeArg.length() - 1)) * 20;
+        } catch (Exception IllegalArgumentException) {
+            Message.get("sidebarmessage-invalid-time").sendF(user, DEFAULT_TIME, DEFAULT_TIME / 20);
+            ticks = DEFAULT_TIME;
+        }
+        return ticks;
+    }
+
+    private void display(String[] args, CommandSender user) {
+        int time = getTicks(args[0], user);
+        SidebarMessage sidebarMessage = new SidebarMessage(args[1]);
+        for (int i = 2; i < args.length; i++) {
+            sidebarMessage.addLine(args[i]);
+        }
+        for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+            sidebarMessage.display(p, time);
+        }
+        String titleWithSpaces = StringUtil.formatText(sidebarMessage.getTitle().replaceAll("_", " "));
+        Message.get("sidebarmessage-display").aSendF(user, titleWithSpaces);
     }
 }
