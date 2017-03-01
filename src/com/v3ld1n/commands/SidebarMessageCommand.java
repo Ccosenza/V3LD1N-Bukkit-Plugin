5bc2abb8a4af15401783deed012f9276663c279d
Ccosenza <ccosenza2@gmail.com>
2017-03-01 08:47:00 -0500

    Removed "s" argument

--- src/com/v3ld1n/commands/SidebarMessageCommand.java
+++ src/com/v3ld1n/commands/SidebarMessageCommand.java
@@ -10,8 +10,6 @@ import com.v3ld1n.util.SidebarMessage;
 import com.v3ld1n.util.StringUtil;
 
 public class SidebarMessageCommand extends V3LD1NCommand {
-    private static final int DEFAULT_TIME = 100;
-
     public SidebarMessageCommand() {
         this.addUsage("<time> <title> <line1> [more lines] ...", "Broadcast a message to all players");
     }
@@ -25,32 +23,24 @@ public class SidebarMessageCommand extends V3LD1NCommand {
             return true;
         }
 
-        display(args, sender);
+        if (!StringUtil.isInteger(args[0])) {
+            Message.get("sidebarmessage-invalid-time").send(sender);
+            return true;
+        }
+        int ticks = Integer.parseInt(args[0]);
+
+        display(args, ticks, sender);
         return true;
     }
 
-    private int getTicks(String timeArg, CommandSender user) {
-        int ticks;
-        try {
-            if (timeArg.charAt(timeArg.length() - 1) != 's') {
-                ticks = Integer.parseInt(timeArg);
-            }
-            ticks = Integer.parseInt(timeArg.substring(0, timeArg.length() - 1)) * 20;
-        } catch (Exception IllegalArgumentException) {
-            Message.get("sidebarmessage-invalid-time").sendF(user, DEFAULT_TIME, DEFAULT_TIME / 20);
-            ticks = DEFAULT_TIME;
-        }
-        return ticks;
-    }
-
-    private void display(String[] args, CommandSender user) {
-        int time = getTicks(args[0], user);
+    // Displays the sidebar to all players
+    private void display(String[] args, int ticks, CommandSender user) {
         SidebarMessage sidebarMessage = new SidebarMessage(args[1]);
         for (int i = 2; i < args.length; i++) {
             sidebarMessage.addLine(args[i]);
         }
         for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-            sidebarMessage.display(p, time);
+            sidebarMessage.display(p, ticks);
         }
         String titleWithSpaces = StringUtil.formatText(sidebarMessage.getTitle().replaceAll("_", " "));
         Message.get("sidebarmessage-display").aSendF(user, titleWithSpaces);
