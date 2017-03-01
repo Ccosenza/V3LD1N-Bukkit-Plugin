a41d026e9b819d9ae808c5a1a6087bd4e70f8209
Ccosenza <ccosenza2@gmail.com>
2017-03-01 11:05:46 -0500

    Rewrite

--- src/com/v3ld1n/commands/WarpCommand.java
+++ src/com/v3ld1n/commands/WarpCommand.java
@@ -13,24 +13,24 @@ import com.v3ld1n.util.Sound;
 public class WarpCommand implements Listener {
     @EventHandler
     public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
-        Player p = event.getPlayer();
-        String message = event.getMessage();
-        String warpName = message.substring(1, message.length());
-        Warp warp = null;
-        for (Warp warpObject : V3LD1N.getWarps()) {
-            if (warpObject.getName().equalsIgnoreCase(warpName)) {
-                warp = warpObject;
-            }
-        }
+        Player player = event.getPlayer();
+        String command = event.getMessage();
+        Warp warp = V3LD1N.getWarp(command.substring(1, command.length()));
+
         if (warp != null) {
             event.setCancelled(true);
-            Bukkit.getServer().dispatchCommand(p, "warp " + warpName);
-            for (Particle particle : warp.getParticles()) {
-                particle.displayToPlayer(p.getLocation(), p);
-            }
-            for (Sound sound : warp.getSounds()) {
-                sound.playToPlayer(p.getLocation(), p);
-            }
+            warp(player, warp);
+        }
+    }
+
+    private void warp(Player player, Warp warp) {
+        Bukkit.getServer().dispatchCommand(player, "warp " + warp.getName());
+
+        for (Particle particle : warp.getParticles()) {
+            particle.displayToPlayer(player.getLocation(), player);
+        }
+        for (Sound sound : warp.getSounds()) {
+            sound.playToPlayer(player.getLocation(), player);
         }
     }
 }
