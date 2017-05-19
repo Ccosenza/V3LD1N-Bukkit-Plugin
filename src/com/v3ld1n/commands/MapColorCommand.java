7972a2f4dc1da83e70b72033aab2b0b66673c75d
Ccosenza <ccosenza2@gmail.com>
2017-05-18 21:28:16 -0400

    Changed /mapcolor to use RGB

--- src/com/v3ld1n/commands/MapColorCommand.java
+++ src/com/v3ld1n/commands/MapColorCommand.java
@@ -3,20 +3,17 @@ package com.v3ld1n.commands;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.MapMeta;
 
 import com.v3ld1n.Message;
 
-import net.minecraft.server.v1_12_R1.NBTTagCompound;
-import net.minecraft.server.v1_12_R1.NBTTagInt;
-
 public class MapColorCommand extends V3LD1NCommand {
-    private static final int LIMIT = 16777215;
+    private static final int LIMIT = 255;
 
     public MapColorCommand() {
-        this.addUsage("<color>", "Set the color of the map you're holding");
+        this.addUsage("<red> <green> <blue>", "Set the color of the map you're holding");
     }
 
     @Override
@@ -25,42 +22,39 @@ public class MapColorCommand extends V3LD1NCommand {
         if (sendNotPlayerMessage(sender)) return true;
         Player p = (Player) sender;
 
-        if (args.length != 1) {
-            sendUsage(sender);
-            return true;
-        }
-
-        int color;
-        try {
-            color = Integer.parseInt(args[0]);
-        } catch (IllegalArgumentException e) {
-            this.sendUsage(sender);
-            return true;
-        }
-
-        if (color < 0 || color > LIMIT) {
-            Message.get("mapcolor-limit").sendF(sender, LIMIT);
-            return true;
-        }
-
         ItemStack item = p.getInventory().getItemInMainHand();
         if (item.getType() != Material.MAP) {
             Message.get("mapcolor-not-map").send(p);
             return true;
         }
-        ItemStack coloredMap = setColor(item, color);
-        p.getInventory().setItemInMainHand(coloredMap);
-        Message.get("mapcolor-set").sendF(p, color);
-        return true;
-    }
 
-    // Sets the map item's color
-    public static ItemStack setColor(ItemStack item, int color) {
-        net.minecraft.server.v1_12_R1.ItemStack stack = CraftItemStack.asNMSCopy(item);
-        NBTTagCompound tag = stack.hasTag() ? stack.getTag() : new NBTTagCompound();
-        NBTTagCompound display = tag.getCompound("display");
-        display.set("MapColor", new NBTTagInt(color));
-        stack.setTag(tag);
-        return CraftItemStack.asBukkitCopy(stack);
+        if (args.length != 3) {
+            sendUsage(sender);
+            return true;
+        }
+
+        int red;
+        int green;
+        int blue;
+        try {
+            red = Integer.parseInt(args[0]);
+            green = Integer.parseInt(args[1]);
+            blue = Integer.parseInt(args[2]);
+        } catch (IllegalArgumentException e) {
+            this.sendUsage(sender);
+            return true;
+        }
+
+        if (red < 0 || red > LIMIT || green < 0 || green > LIMIT || blue < 0 || blue > LIMIT) {
+            Message.get("mapcolor-limit").sendF(sender, LIMIT);
+            return true;
+        }       
+        
+        MapMeta map = (MapMeta) item.getItemMeta();
+        map.setColor(org.bukkit.Color.fromRGB(red, green, blue));
+        item.setItemMeta(map);
+
+        Message.get("mapcolor-set").sendF(p, red, green, blue);
+        return true;
     }
 }
