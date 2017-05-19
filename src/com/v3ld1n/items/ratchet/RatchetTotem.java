b04f31b3cd79ffd207dd0f2db390b67f3b143925
Ccosenza <ccosenza2@gmail.com>
2017-05-19 13:38:14 -0400

    Changed to healing

--- src/com/v3ld1n/items/ratchet/RatchetTotem.java
+++ src/com/v3ld1n/items/ratchet/RatchetTotem.java
@@ -1,10 +1,15 @@
 package com.v3ld1n.items.ratchet;
 
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
 
+import com.v3ld1n.Message;
 import com.v3ld1n.items.V3LD1NItem;
+import com.v3ld1n.util.EntityUtil;
 
 public class RatchetTotem extends V3LD1NItem {
     public RatchetTotem() {
@@ -12,17 +17,25 @@ public class RatchetTotem extends V3LD1NItem {
     }
 
     @EventHandler
-    public void onInteract(PlayerInteractEvent event) {
+    public void onInteractEntity(PlayerInteractEntityEvent event) {
         Player player = event.getPlayer();
         if (!entityIsHoldingItem(player)) return;
-        if (!isRightClick(event.getAction(), event.getHand())) return;
+        if (event.getHand() != EquipmentSlot.HAND) return;
+        if (!(event.getRightClicked() instanceof LivingEntity)) return;
 
-        push(player);
+        event.setCancelled(true);
+        heal(player, (LivingEntity) event.getRightClicked());
     }
 
-    private void push(Player player) {
-        player.setVelocity(player.getVelocity().setY(settings.getDouble("speed")));
-        displayParticles(player.getLocation());
-        playSounds(player.getLocation());
+    private void heal(Player player, LivingEntity entity) {
+        int oldHealth = (int) entity.getHealth();
+        EntityUtil.heal(entity, settings.getDouble("heal-amount"));
+        int newHealth = (int) entity.getHealth();
+        int maxHealth = (int) entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+        Message.get("item-ratchetstotem-heal").aSendF(player, entity.getName(), newHealth, maxHealth);
+        playSounds(entity.getEyeLocation());
+        if (oldHealth != newHealth) {
+            displayParticles(entity.getEyeLocation());
+        }
     }
 }
